e99481abc3062cef6b2b2ee505e077a9c850e558
gilszon <gilszon@gmail.com>
2022-01-19 12:55:10 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,17 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "../../utils/requests";
 
 function Listing() {
+
+    // WRONG WAY
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then( response => {
+        console.log(response.data);
+    });
+
 
     return (
         <>
