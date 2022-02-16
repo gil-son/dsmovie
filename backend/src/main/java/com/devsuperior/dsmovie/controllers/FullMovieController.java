4d422f75e3de3102dd1aa3f729992d806086e946
gilszon <gilszon@gmail.com>
2022-02-16 17:59:46 -0300

    Organized

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/FullMovieController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/FullMovieController.java
@@ -9,14 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmovie.dto.FullMovieDTO;
-import com.devsuperior.dsmovie.dto.MovieDTO;
-import com.devsuperior.dsmovie.dto.MovieLanguageDTO;
-import com.devsuperior.dsmovie.entities.Movie;
-import com.devsuperior.dsmovie.entities.MovieLanguage;
 import com.devsuperior.dsmovie.repositories.FullMovieRepository;
-import com.devsuperior.dsmovie.repositories.MovieJoinRepository;
-import com.devsuperior.dsmovie.repositories.MovieLanguageRepository;
-import com.devsuperior.dsmovie.repositories.MovieRepository;
+
 
 @RestController
 @RequestMapping(value = "/full-movies-titles")
@@ -26,40 +20,10 @@ public class FullMovieController {
 	FullMovieRepository fullMovieRepository;
 	
 	
-	
-	
-	
 	@GetMapping
 	public Page<FullMovieDTO> findAll(Pageable pageable){
 		Page<FullMovieDTO> result = fullMovieRepository.findAllPaged(pageable);
 		return result;
 	}
-	 
-	/*
-	@GetMapping
-	public Page<String> findAll(Pageable pageable){
-		Page<String> result = movieJoinRepository.findAllTitlesPaged(pageable);
-		
-		return result;
-	}
-	*/
-	
-	/*
-	@GetMapping
-	public Page<String> findAll(Pageable pageable){
-		Page<String> result = movieJoinRepository.findAll(pageable);
-		
-		return result;
-	}
-	*/
-	
-	/*
-	@GetMapping
-	public Page<String> findAll(Pageable pageable){
-		Page<String> result = fullmovieRepository.findAll(pageable);
-		
-		return result;
-	}
-	*/
 	
 }
